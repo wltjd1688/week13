4232900b72b28ebcb8cee38cad3848a603f1c922
wltjd1688 <wltjd1688@gmail.com>
2023-11-07 08:33:54 +0900

    [add]

--- jungle-react/src/pages/Singup.js
+++ jungle-react/src/pages/Singup.js
@@ -44,11 +44,11 @@ const Singup = () => {
       </div>
       <div className=" px-3 py-1 w-full border-2 whitespace-nowrap inline-grid my-3">
         <label htmlFor="pw">password</label>
-        <input type="password" className="outline-none hover:bg-slate-100" onChange={(e)=>{setPassword(e.target.value)}} type="text" name="pw" id="pw" placeholder="password"/>
+        <input type="password" className="outline-none hover:bg-slate-100" onChange={(e)=>{setPassword(e.target.value)}} name="pw" id="pw" placeholder="password"/>
       </div>
       <div className=" px-3 py-1 w-full border-2 whitespace-nowrap inline-grid my-3">
         <label htmlFor="cpw">confirm password</label>
-        <input type="password" className="outline-none hover:bg-slate-100" onChange={(e)=>{setPassword(e.target.value)}} type="text" name="cpw" id="cpw" placeholder="password"/>
+        <input type="password" className="outline-none hover:bg-slate-100" onChange={(e)=>{setPassword(e.target.value)}} name="cpw" id="cpw" placeholder="password"/>
       </div>
       <div className="flex justify-center">
         <button onClick={(e)=>{handleSingUp(e)}} type='login' className=" mt-3 px-6 py-1 rounded-md border-2 hover:bg-slate-100 active:bg-slate-300">회원가입</button>
