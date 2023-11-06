7d985fbb10d30722be01715c070bb7cea1bb3058
wltjd1688 <wltjd1688@gmail.com>
2023-11-07 08:58:46 +0900

    [add]

--- jungle-react/src/pages/Home.js
+++ jungle-react/src/pages/Home.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Home() {
   return(
     <>
-      <div className=" border-">
+      <div className=" flex justify-center border-2 rounded-md">
         <Link to={"/boards"}>게시판 가는길</Link>
       </div>
     </>
